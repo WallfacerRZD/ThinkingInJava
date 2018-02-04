24509c7d90f8e9b4014c01433a374b379fbcb9ef
WallfacerRZD <1048058400@qq.com>
2018-02-05 01:10:22 +0800

    静态域

---
+++ src/chapter5/StaticField.java
@@ -0,0 +1,25 @@
+package chapter5;
+
+class T {
+    T(int x) {
+        System.out.println("T()" + x);
+    }
+}
+
+/**
+ * 输出: T()1
+ *      T()2
+ * t 的初始化在定义t时进行
+ * 静态块中并不是初始化, 只是简单的赋值.
+ */
+public class StaticField {
+    static T t = new T(1);
+
+    static {
+        t = new T(2);
+    }
+
+    public static void main(String[] args) {
+        new StaticField();
+    }
+}
