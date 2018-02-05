d7dd344ccdc50af9ded1139a0e7908d9569c1170
WallfacerRZD <1048058400@qq.com>
2018-02-05 16:36:25 +0800

    向下转型

---
+++ src/chapter8/Sub1.java
@@ -0,0 +1,27 @@
+package chapter8;
+
+class A {
+}
+
+class B extends A {
+}
+
+class Super1 {
+    A f() {
+        System.out.println("Super1.f()");
+        return new A();
+    }
+}
+
+public class Sub1 extends Super1 {
+    @Override
+    B f() {
+        System.out.println("Sub1.f()");
+        return new B();
+    }
+
+    public static void main(String[] args) {
+        Super1 a = new Sub1();
+        a.f();
+    }
+}
