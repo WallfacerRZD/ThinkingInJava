3878f39de694e9999affdc7efe5d7c091de3d644
WallfacerRZD <1048058400@qq.com>
2018-02-05 15:33:19 +0800

    在任何事物发生之前, 将分配给对象的存储空间初始化为二进制零

---
+++ src/chapter8/RoundGlyph.java
@@ -0,0 +1,38 @@
+package chapter8;
+
+class Glyph {
+    void draw() {
+        System.out.println("Glyph.draw()");
+    }
+    Glyph() {
+        System.out.println("Glyph() before draw()");
+        // 创建子类时会调用子类的draw
+        // 且此时子类还未初始化(子类的内存里是二进制0)
+        draw();
+        System.out.println("Glyph() after draw()");
+    }
+}
+
+/**
+ *输出:
+ *   Glyph() before draw()
+ *   RoundGlyph .draw() radius = 0
+ *   Glyph() after draw()
+ *   RoundGlyph() radius = 5
+ */
+public class RoundGlyph extends Glyph{
+    private int radius = 1;
+
+    @Override
+    void draw() {
+        System.out.println("RoundGlyph .draw() radius = " + radius);
+    }
+    RoundGlyph(int r) {
+        radius = r;
+        System.out.println("RoundGlyph() radius = " + radius);
+    }
+
+    public static void main(String[] args) {
+        new RoundGlyph(5);
+    }
+}
