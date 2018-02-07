4d881b562c3e86ababb762e934c963e3b33c8dab
WallfacerRZD <1048058400@qq.com>
2018-02-07 21:34:23 +0800

    泛型方法

---
+++ src/chapter15/GenericMethods.java
@@ -0,0 +1,17 @@
+package chapter15;
+
+public class GenericMethods {
+    public <T> void f(T x) {
+        System.out.println(x.getClass().getName());
+    }
+
+    public static void main(String[] args) {
+        GenericMethods gm = new GenericMethods();
+        gm.f(1);
+        gm.f(1.0);
+        gm.f(1L);
+        gm.f(1F);
+        gm.f('c');
+        gm.f(gm);
+    }
+}
---
+++ src/chapter15/README.md
@@ -0,0 +1,8 @@
+# 泛型
+基本类型无法作为类型参数   
+
+## 泛型方法
+是否拥有泛型方法, 与所在类是否是泛型没有关系  
+
+[GenericMethods.java](./GenericMethods.java)  
+
