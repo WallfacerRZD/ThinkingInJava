3395ebecdbcbf7286a845a6d155b37a0ec9e3ff3
WallfacerRZD <1048058400@qq.com>
2018-02-07 11:50:54 +0800

    toString()的递归调用

---
+++ src/chapter13/InfiniteRecursion.java
@@ -0,0 +1,22 @@
+package chapter13;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InfiniteRecursion {
+    @Override
+    public String toString() {
+        // 拼接字符串时会调用toString()方法, 产生递归调用
+        // 正确做法是使用Object.toString()方法.
+        return " InfiniteRecursion address: " + this + "\n";
+    }
+
+    public static void main(String[] args) {
+        List<InfiniteRecursion> v = new ArrayList<>();
+        for (int i = 0; i < 10; ++i) {
+            v.add(new InfiniteRecursion());
+        }
+
+        System.out.println(v);
+    }
+}
