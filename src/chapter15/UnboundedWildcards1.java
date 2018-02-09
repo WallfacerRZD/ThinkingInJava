1aef6f46ca2219b79d2bb83e6825718899374bb6
WallfacerRZD <1048058400@qq.com>
2018-02-09 21:27:05 +0800

    UnboundedWildcard

---
+++ src/chapter15/UnboundedWildcards1.java
@@ -0,0 +1,47 @@
+package chapter15;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UnboundedWildcards1 {
+    static List list1;
+    static List<?> list2;
+    static List<? extends Object> list3;
+
+    static void assign1(List list) {
+        list1 = list;
+        list2 = list;
+        // Warning: Unchecked conversion
+        list3 = list;
+    }
+
+    static void assgin2(List<?> list) {
+        list1 = list;
+        list2 = list;
+        list3 = list;
+    }
+
+    static void assgin3(List<? extends Object> list) {
+        list1 = list;
+        list2 = list;
+        list3 = list;
+    }
+    public static void main(String[] args) {
+        assign1(new ArrayList());
+        assgin2(new ArrayList());
+        // Warning: unchecked conversion
+        // 编译器处理<? extends Object>与<?>是不同的
+        assgin3(new ArrayList());
+
+        assign1(new ArrayList<String>());
+        assgin2(new ArrayList<String>());
+        assgin3(new ArrayList<String>());
+
+        List<?> wildList = new ArrayList();
+        wildList = new ArrayList<String>();
+        assign1(wildList);
+        assgin2(wildList);
+        assgin3(wildList);
+    }
+}
