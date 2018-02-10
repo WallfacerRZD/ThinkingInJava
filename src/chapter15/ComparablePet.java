c498a6448c66e607cf82786c70142337061d15f6
WallfacerRZD <1048058400@qq.com>
2018-02-10 12:15:58 +0800

    基类劫持

---
+++ src/chapter15/ComparablePet.java
@@ -0,0 +1,14 @@
+package chapter15;
+
+public class ComparablePet implements Comparable<ComparablePet>{
+
+    @Override
+    public int compareTo(ComparablePet o) {
+        return 0;
+    }
+}
+
+// 无法编译
+/*class Hamster extends ComparablePet implements Comparable<Hamster> {
+
+}*/
